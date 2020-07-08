eed8b76c3b0d7504bc1d57220a685a65997a5b57
Frank Xin <frankxin10@gmail.com>
2020-07-08 15:58:51 +0800

    Initial commit

---
+++ bot.py
@@ -0,0 +1,63 @@
+import praw
+from configs import settings
+
+reddit = praw.Reddit(client_id=settings.get("client_id"),
+                     client_secret=settings.get("client_secret"),
+                     user_agent=settings.get("user_agent"),
+                     username=settings.get("username"),
+                     password=settings.get("password"))
+
+subreddit = reddit.subreddit(settings.get("subreddit"))
+
+
+def nudgeUsers():
+    '''
+    Searches through the comments of the most recent submissions for keyword !Nudge and nudges users through private messaging 
+    '''
+    for submission in subreddit.new(limit=36):
+        for comment in submission.comments:
+            if hasattr(comment, "body"):
+                if ("!Nudge" in comment.body):
+                    sentenceList = comment.body.split()
+                    #Edge check if last word of the comment is !Nudge which results in an invalid user
+                    if (sentenceList[-1] == "!Nudge"):
+                        return
+                    author = comment.author
+                    #Avoids infinite response to keyword
+                    if (author == "NudgeBot"):
+                        return
+                    #Avoids duplicate responses to same comment containing keyword
+                    for response in comment.replies:
+                        if hasattr(response, "body"):
+                            if (response.author == "NudgeBot"):
+                                return
+                    usernameIndex = sentenceList.index("!Nudge")
+                    receivingUser = sentenceList[usernameIndex+1]
+                    #Corrects Reddit name formatting for underscores
+                    receivingUser = receivingUser.replace("\\_", "_")
+                    comment.reply("You nudged /u/{}".format(receivingUser))
+                    reddit.redditor(receivingUser).message("{} nudged you".format(author), "{}".format(comment.permalink))
+                    print("Nudge successful")
+
+
+def instruct():
+    '''
+    Searches through the comments of the most recent submissions and comments the instructions for the bot if the comment only contains !NudgeBot
+    '''
+    for submission in subreddit.new(limit=36):
+        for comment in submission.comments:
+            if hasattr(comment, "body"):
+                if ("!NudgeBot" in comment.body):
+                    sentence = "".join(comment.body.split())
+                    if (sentence == "!NudgeBot"):
+                        comment.reply("Hi! You can comment !NudgeBot [Username_of_user_here (without the /u/)] under a post to nudge someone :)")
+
+
+def main():
+    nudgeUsers()
+    instruct()
+
+
+if __name__ == "__main__":
+    main()
+
