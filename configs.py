7baba0629ba06b1571a30d24f00f0ca1bc0f5e15
Frank Xin <frank01px2021@saschina.org>
2020-07-08 16:01:55 +0800

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,8 @@
+settings = {
+    "client_id": "Found at https://www.reddit.com/prefs/apps/",
+    "client_secret": "Found at https://www.reddit.com/prefs/apps/",
+    "user_agent": "Follow Praw Documentation to Name Agent",
+    "username": "Bot Username",
+    "password": "Bot Password",
+    "subreddit": "Subreddit Name"
+}
